49c2a989385842c37eb2a2d24c5cc0b41cea05da
Mikael <mikael.wilfeldt@gmail.com>
2020-04-15 10:00:13 +0200

    Added version 0.1

---
+++ Fence/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Fence
+{
+    public class Program
+    {
+        public const int RibWidth = 45;
+        public const int PreferedSpace = 70;
+
+        public static void Main(string[] args)
+        {
+            var totalRibs = 0;
+
+            foreach (var sectionWidth in Sections.Data)
+            {
+                var ribs = CalculateNumberOfRibs(sectionWidth);
+                var space = CalculateActualSpace(sectionWidth, ribs);
+                var cc = (double)sectionWidth / 2;
+                totalRibs += ribs;
+
+                Console.WriteLine($"Ribs: {ribs}, Space: {space}, CC: {cc}");
+            }
+
+            Console.WriteLine($"Total amount of ribs: {totalRibs}");
+        }
+
+        private static int CalculateNumberOfRibs(int sectionWidth)
+        {
+            var x = (double)(sectionWidth - ((2 * PreferedSpace) + RibWidth)) / (RibWidth + PreferedSpace);
+
+            return (int)Math.Round(x) + 1;
+        }
+
+        private static double CalculateActualSpace(int sectionWidth, int numberOfRibs)
+        {
+            var x = sectionWidth - ((numberOfRibs - 1) * (RibWidth + PreferedSpace)) - RibWidth;
+            var difference = (PreferedSpace * 2) - x;
+            var space = (double)difference / (numberOfRibs - 1);
+
+            return Math.Round(PreferedSpace + space, 1);
+        }
+    }
+}
---
+++ Fence/Sections.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Fence
+{
+    public static class Sections
+    {
+        public static IList<int> Data = new List<int>
+        {
+            1460,
+            1540,
+            1535,
+            1630,
+            1585,
+            1660,
+            1635,
+            1630,
+            1620,
+            1575,
+            1575,
+            1620,
+            1780,
+            1850,
+            1800
+        };
+    }
+}
